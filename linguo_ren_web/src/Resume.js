e9962932c3de998e1b4bedeee483e2a8b011ba46
linguoren369 <lren@bowdoin.edu>
2025-01-04 00:33:24 -0500

    okay

--- linguo_ren_web/src/Resume.js
+++ linguo_ren_web/src/Resume.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import LinguoRenPDF from './data/Linguo_Ren.pdf'; // Import your PDF file
 
 const Resume = () => {
   return (
-    <div>
-      <h1>Download My Resume</h1>
-
-      {/* Download Button */}
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Resume</h1>
+      <iframe
+        src="https://www.nytimes.com/" // Correct path to the PDF in the public folder
+        title="Resume"
+        width="90%"
+        height="600px"
+        style={{
+          border: '1px solid #ccc',
+          borderRadius: '5px',
+          margin: '20px auto',
+        }}
+      ></iframe>
       <a
-        href={LinguoRenPDF}
+        href="/Linguo_Ren.pdf" // Direct download link to the PDF in the public folder
         download="Linguo_Ren.pdf"
-        className="download-button"
         style={{
           display: 'inline-block',
           marginTop: '20px',
