bf07215ddcc67bf018bde6ec057a2bf314408654
linguoren369 <lren@bowdoin.edu>
2025-01-05 09:03:46 -0500

    Fix GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './styles/index.css'; // Your custom global styles
+import App from './App'; // Main application component
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS for styling
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Bootstrap JS for components like modals and dropdowns
 
+// Create the root and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
