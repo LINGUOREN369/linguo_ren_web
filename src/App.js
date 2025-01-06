c50419baa36cd2f46f69a29479a9493ab2559446
linguoren369 <lren@bowdoin.edu>
2025-01-05 23:12:54 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
 import Project from './Project';
 import Resume from './Resume';
-const profile_photo = process.env.PUBLIC_URL + '/docs/profile_photo.JPG';
+const profilePhoto = process.env.PUBLIC_URL + '/docs/profile_photo.JPG';
 const favicon = process.env.PUBLIC_URL + '/docs/favicon.ico';
 
 function AppContent() {
   const [showHeader, setShowHeader] = useState(true);
-  const location = useLocation(); // Get the current route location
+  const location = useLocation();
 
   useEffect(() => {
-    // Update the tab title based on the route
     switch (location.pathname) {
       case '/resume':
         document.title = "Linguo's Résumé";
@@ -22,176 +22,155 @@ function AppContent() {
       default:
         document.title = "Linguo's Portfolio";
     }
-  
-    // Dynamically update the favicon
+
     let faviconLink = document.querySelector("link[rel='icon']");
     faviconLink.href = favicon;
-  
   }, [location]);
 
   const handleNavigation = (showHeader) => {
     setShowHeader(showHeader);
-  };
-
-  const sidebarStyle = {
-    fontSize: '30px',
-    fontWeight: 'bold',
-    lineHeight: '2',
-    width: '15%',
-    padding: '20px',
-  };
-
-  const linkStyle = {
-    color: '#ffffff',
-    textDecoration: 'none',
-  };
-
-  const linkHoverStyle = (e) => (e.target.style.textDecoration = 'underline');
-  const linkUnhoverStyle = (e) => (e.target.style.textDecoration = 'none');
-
-  const bowdoinStyle = {
-    color: '#4da8da',
-    textDecoration: 'none',
-    fontWeight: 'bold',
+    document.querySelector('.navbar-collapse').classList.remove('show');
   };
 
   return (
     <div className="App">
-      <div className="layout">
-        {/* Left Section */}
-        <aside className="sidebar" style={sidebarStyle}>
-          <nav>
-            <ul>
-              <li>
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+        <button
+  className="navbar-toggler me-auto" /* Add 'me-auto' for left alignment */
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarNav"
+  aria-controls="navbarNav"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span className="navbar-toggler-icon"></span>
+</button>
+          <Link className="navbar-brand" to="/">
+            Linguo Ren
+          </Link>
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
                 <Link
                   to="/"
                   onClick={() => handleNavigation(true)}
-                  style={linkStyle}
-                  onMouseOver={linkHoverStyle}
-                  onMouseOut={linkUnhoverStyle}
+                  className="nav-link"
                 >
                   Home
                 </Link>
               </li>
-              <li>
+              <li className="nav-item">
                 <Link
                   to="/resume"
                   onClick={() => handleNavigation(false)}
-                  style={linkStyle}
-                  onMouseOver={linkHoverStyle}
-                  onMouseOut={linkUnhoverStyle}
+                  className="nav-link"
                 >
                   Résumé
                 </Link>
               </li>
-              <li>
+              <li className="nav-item">
                 <Link
                   to="/project"
                   onClick={() => handleNavigation(false)}
-                  style={linkStyle}
-                  onMouseOver={linkHoverStyle}
-                  onMouseOut={linkUnhoverStyle}
+                  className="nav-link"
                 >
                   Projects
                 </Link>
               </li>
-              <li>
+              <li className="nav-item">
                 <a
                   href="https://www.linkedin.com/in/linguo-ren/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  style={linkStyle}
-                  onMouseOver={linkHoverStyle}
-                  onMouseOut={linkUnhoverStyle}
+                  className="nav-link"
                 >
                   LinkedIn
                 </a>
               </li>
-              <li>
+              <li className="nav-item">
                 <a
                   href="https://github.com/LINGUOREN369?tab=repositories"
                   target="_blank"
                   rel="noopener noreferrer"
-                  style={linkStyle}
-                  onMouseOver={linkHoverStyle}
-                  onMouseOut={linkUnhoverStyle}
+                  className="nav-link"
                 >
                   GitHub
                 </a>
               </li>
-              <li>
-                <a
-                  href="mailto:lren@bowdoin.edu"
-                  style={linkStyle}
-                  onMouseOver={linkHoverStyle}
-                  onMouseOut={linkUnhoverStyle}
-                >
+              <li className="nav-item">
+                <a href="mailto:lren@bowdoin.edu" className="nav-link">
                   Contact
                 </a>
               </li>
             </ul>
-          </nav>
-        </aside>
+          </div>
+        </div>
+      </nav>
 
-        {/* Right Section */}
-        <main className="content">
-          {showHeader && location.pathname !== '/resume' && ( // Conditionally render header
-            <header>
-              <h1>Linguo Ren</h1>
-              <h2>Aspiring Data Analyst & Consulting Analyst</h2>
+      {/* Main Content */}
+      <div className="container mt-4">
+        {showHeader && location.pathname !== '/resume' &&(
+          <header className="row align-items-center">
+            <div className="col-lg-4 text-center">
+            <img
+              src={profilePhoto}
+              alt="Linguo Ren"
+              className="profile-image img-fluid mt-2"
+            />
+            </div>
+            {/* Text Section */}
+            <div className="col-lg-8">
+            
+              {/* add some padding */} 
+              <div className="mt-4"></
+              div>
+
+              
+              <h1>Hey, I'm Linguo,</h1>
               <p>
-                I’m a passionate and driven individual pursuing studies in <strong>Computer Science, Mathematics, and Education at</strong>{' '}
+                I’m a passionate individual pursuing <strong>Computer Science, Mathematics, and Education</strong> at{' '}
                 <a
                   href="https://www.bowdoin.edu"
                   target="_blank"
                   rel="noopener noreferrer"
-                  aria-label="Bowdoin College Official Website"
-                  style={bowdoinStyle}
+                  className="text-info"
                 >
                   Bowdoin College
                 </a>
-                . My academic journey is driven by a deep curiosity for problem-solving and a passion for interdisciplinary connections.
+                .
+            
+              My academic journey is driven by a deep curiosity for problem-solving and a passion for interdisciplinary connections.
               </p>
 
               <p>
-                With a strong foundation in data science, machine learning, deep learning, and a well-rounded liberal arts education, I combine technical expertise with a commitment to understanding and addressing complex systems. I aim to bridge analytical rigor with strategic insight, focusing on real-world challenges by analyzing comprehensive datasets to uncover impactful, data-driven solutions.
+              With a strong foundation in data science, machine learning, deep learning, and a well-rounded liberal arts education, I combine technical expertise with a commitment to understanding and addressing complex systems. 
               </p>
 
               <p>
-                <strong>Technical Skills:</strong>
-                <ul>
-                  <li><strong>Data Analysis:</strong> Python (Pandas, NumPy, Scikit-Learn, Matplotlib, Seaborn, NLTK, PyTorch), R (RStudio, Tidyverse, ggplot2), SQL, Stata, LaTeX</li>
-                  <li><strong>Web & Software Development:</strong> Python, Java, C, HTML, JavaScript, CSS, React, Node.js</li>
-                  <li><strong>Tools & Platforms:</strong> Microsoft Suite (Excel, PowerPoint, Word), GitHub, Anaconda, Visual Studio Code, RStudio</li>
-                </ul>
+              I aim to bridge analytical rigor with strategic insight, focusing on real-world challenges by analyzing comprehensive datasets to uncover impactful, data-driven solutions.
               </p>
-              <p>
-                <strong>Languages:</strong> Chinese (Native), English (Bilingual Proficiency), Spanish (Intermediate)
-              </p>
-              <p>
-                <strong>Hobbies:</strong> Triathlon, Marathon, Bikepacking, Cycling, Ice Hockey, Lacrosse, Hiking, Skiing, Jazz Band
-              </p>
-              <Link to="/project" onClick={() => handleNavigation(false)}>
+
+              <div className="mt-3">
+              <Link to="/project">
                 <button className="portfolio-button">View My Projects</button>
               </Link>
-
-              <Link to="/resume" onClick={() => handleNavigation(false)}>
+              <Link to="/resume">
                 <button className="portfolio-button2">View My Resume</button>
               </Link>
-
-              <img
-                src={profile_photo}
-                alt="Linguo Ren"
-                className="profile-image"
-              />
-            </header>
-          )}
-
-          <Routes>
-            <Route path="/project" element={<Project />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
-        </main>
+            </div>
+            </div>
+            {/* Image Section */}
+            
+          </header>
+        )}
+        <Routes>
+          <Route path="/project" element={<Project />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'; // Main application component
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS for styling
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Bootstrap JS for components like modals and dropdowns
 
+
 // Create the root and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
